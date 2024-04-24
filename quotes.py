77f78865f4e4f53811546b065c2cb0a19a7be82d
avafox <avafoxemail@gmail.com>
2024-04-24 14:48:41 -0400

    Add general structure for how quotes.py works

--- quotes.py
+++ quotes.py
@@ -12,7 +12,7 @@ quotes = {
         "caleb": {
             "quotes": {
                 "quote": "",
-                "isChalk": bool,
+                "isChalk": [bool, bool], # First bool = if chalk instance, second is whether or not he dropped it
                 "isAction": bool,
                 "timestamp": ""
                 },
@@ -20,7 +20,7 @@ quotes = {
         "al": {
             "quotes": {
                 "quote": "",
-                "isChalk": bool,
+                "isChalk": [bool, bool], # Is just gonna be no
                 "isAction": bool,
                 "timestamp": ""
                 }
@@ -46,3 +46,64 @@ while quotes_remaining:
         quotes_remaining = False
         break
     
+    # this is... fucking hilarious
+    options = ["y", "n"]
+    while True:
+        author = input("Is this caleb? (y/n): ")
+        if author in options:
+            if author == "y":
+                author = "caleb"
+            else:
+                author = "al"
+
+            # for quote in quotes.keys():
+            #     if quote["author"] == author:
+
+            break
+    if author == "caleb":
+        while True:
+            is_chalk = input("Does it pertain to chalk? y/n: ")
+            if is_chalk in options:
+                if is_chalk == "y":
+                    # Gonna be isChalk[0] = True
+                    is_chalk = True
+                    while True:
+                        # Check if it's a dropped instance
+                        dropped = input("Did he drop the chalk? (y/n): ")
+                        if dropped in options:
+                            if dropped == "y":
+                                # Gonna be like...isChalk[1] = True
+                                dropped = True
+                            else:
+                                dropped = False
+                            break 
+                else:
+                    is_chalk = False
+                break
+
+        while True:
+            is_action = input("Is it an action? (y/n): ")
+            if is_action in options:
+                if is_action == "y":
+                    is_action = True
+                    description = input("Description of action: \n")
+                else:
+                    is_action = False
+                    description = None
+                break
+
+        while True:
+            has_timestamp = input("Got timestamp? (y/n): ")
+            if has_timestamp in options:
+                if has_timestamp == "y":
+                    timestamp = input("Insert timestamp: ")
+                else:
+                    timestamp = None
+                break
+    else:
+        is_chalk = False
+        dropped = False
+        is_action = False
+        description = None
+        timestamp = None
+    print(f"author: {author}\n quote: {current_quote}\n is chalk: {is_chalk} \n dropped chalk? {dropped}\n is action: {is_action}\n action description: {description}\n timestamp: {timestamp}\n")
