7c7cfc074c3ca5572dcbb6cf50c26540d4764041
Ava Fox <avafoxemail@gmail.com>
2024-06-24 12:44:54 -0700

    Change port

--- src/app.js
+++ src/app.js
@@ -128,5 +128,8 @@ app.post("/add", (req, res) => {
   res.sendStatus(303)
 })
 
-console.log('now listening on http://localhost:3000')
-app.listen(3000)
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
